d76f4d3029d39136be49517e6b54aad8080e2d6c
Alex Ren <alex.ren@gmail.com>
2024-01-07 10:44:14 -0800

    add loaders

---
+++ PyraUnity/Assets/Scripts/LoadBundle.cs
@@ -0,0 +1,45 @@
+using UnityEditor;
+using UnityEngine;
+
+public class LoadBundle : MonoBehaviour
+{
+    [Tooltip("Keeping around for debugging purposes.")]
+    public GameObject loadedPrefab;
+    
+    [Tooltip("Keeping around for debugging purposes.")]
+    public GameObject createdObject;
+    
+    public void LoadAssetBundle()
+    {
+        var bundlePathToLoad = EditorUtility.OpenFilePanelWithFilters("Choose a bundle to load", Application.streamingAssetsPath, new[] {"AssetBundle", "bundle"});
+        if (bundlePathToLoad == null)
+        {
+            Debug.Log("no path to asset bundle?");
+            return;
+        }
+        
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(bundlePathToLoad);
+
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        
+        var names = myLoadedAssetBundle.GetAllAssetNames();
+        
+        
+        loadedPrefab = myLoadedAssetBundle.LoadAsset<GameObject>(names[0]);
+        createdObject = Instantiate(loadedPrefab);
+        PrefabUtility.SaveAsPrefabAsset(createdObject, System.IO.Path.Combine("Assets", "AssetName.prefab"));
+    }
+
+    public void UnloadAssetBundles()
+    {
+        AssetBundle.UnloadAllAssetBundles(true);
+        if (createdObject != null)
+        {
+            DestroyImmediate(createdObject);
+        }
+    }
+}
